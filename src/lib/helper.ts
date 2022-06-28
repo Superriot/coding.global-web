2d51ee4239442d07786c8e3821041f385db8bd0a
Don Cryptus <don.cryptus@gmail.com>
2022-06-28 11:45:33 +0200

    fix: vercel url

--- src/lib/helper.ts
+++ src/lib/helper.ts
@@ -19,7 +19,7 @@ export function openGraph({
     : undefined;
   const ogDesc = encodeURIComponent(description.trim());
 
-  const url = `https://og-don-cryptus.vercel.app/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
+  const url = `https://og-xi.vercel.app/api/general?siteName=${ogSiteName}&description=${ogDesc}&logo=${ogLogo}${
     ogTemplateTitle ? `&templateTitle=${ogTemplateTitle}` : ''
   }`;
   console.log(url);
