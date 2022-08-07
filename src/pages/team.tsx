61c52ff866f7979c0adaccde6524d9c8ec3b2777
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-08-07 23:49:36 +0200

    feat: asdf

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -5,7 +5,12 @@ export default function Team() {
     <Layout>
       <main>
         <section className='layout my-10 rounded-md bg-dark-light bg-opacity-80 p-10 text-white'>
-          <div>aldn</div>
+          <h1>Team</h1>
+          <div className='flex justify-between'>
+            <div className=''>01</div>
+            <div className=''>02</div>
+            <div className=''>03</div>
+          </div>
         </section>
       </main>
     </Layout>
