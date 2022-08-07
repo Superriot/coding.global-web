30c119c5b08109eb7545d05cb7c90f7dfdc9027d
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-08-08 00:24:50 +0200

    feat: sdf

--- src/pages/team.tsx
+++ src/pages/team.tsx
@@ -4,12 +4,12 @@ export default function Team() {
   return (
     <Layout>
       <main>
-        <section className='layout my-10 rounded-md bg-dark-light bg-opacity-80 p-10 text-white'>
-          <h1>Team</h1>
-          <div className='flex justify-between'>
-            <div className=''>01</div>
-            <div className=''>02</div>
-            <div className=''>03</div>
+        <section className='layout my-10 justify-center rounded-md bg-dark-light bg-opacity-80 p-10 text-white '>
+          <h1 className='text-center'>Our Team</h1>
+          <div className='mt-8 flex justify-between'>
+            <div className='h-90 box-content w-60 rounded-3xl border-4 p-4 hover:drop-shadow-2xl'></div>
+            <div className='box-content h-80 w-60 rounded-3xl border-4 p-4'></div>
+            <div className='box-content h-80 w-60 rounded-3xl border-4 p-4'></div>
           </div>
         </section>
       </main>
