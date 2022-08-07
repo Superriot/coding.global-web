06d122e6d6cf8378d591a74c456e07c36f923ea2
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-08-07 22:03:30 +0200

    feat: team page

---
+++ src/pages/team.tsx
@@ -0,0 +1,13 @@
+import Layout from '@/components/layout/Layout';
+
+export default function Team() {
+  return (
+    <Layout>
+      <main>
+        <section className='layout my-10 rounded-md bg-dark-light bg-opacity-80 p-10 text-white'>
+          <div>aldn</div>
+        </section>
+      </main>
+    </Layout>
+  );
+}
