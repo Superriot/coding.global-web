c36fb326f9ba7a966cdee721af7fcc914606ba67
Don-Cryptus <don.cryptus@gmail.com>
2022-08-14 21:52:25 +0200

    chore: add sidebar example

---
+++ src/pages/bot.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Sidebar from '@/components/layout/Sidebar';
+
+const Bot: React.FC = () => {
+  return (
+    <Sidebar>
+      <div>asd</div>
+    </Sidebar>
+  );
+};
+
+export default Bot;
