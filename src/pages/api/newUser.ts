5ce248571f951f5501084de8b8dc8c6496e488b9
Don-Cryptus <don.cryptus@gmail.com>
2022-08-20 20:25:08 +0200

    fix: refactor

--- src/pages/api/newUser.ts
+++ src/pages/api/newUser.ts
@@ -1,34 +1,12 @@
 /* eslint-disable unused-imports/no-unused-vars */
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import fs from 'fs';
-
-type User = { email: string; password: string; confirmPassword: string };
 
 function handler(req: any, res: any) {
   if (req.method === 'POST') {
-    const data = req.body as User;
+    const data = req.body;
 
-    let users: User[] | undefined;
-
-    if (fs.existsSync('user.json')) {
-      users = JSON.parse(fs.readFileSync('user.json', 'utf-8')) as User[];
-    }
-
-    if (!users) {
-      fs.writeFileSync('user.json', JSON.stringify([data]));
-      return res.status(200).json(data);
-    }
-    console.log(users);
-
-    const user = users.find((user) => user.email === data.email);
-
-    if (user) {
-      return res.status(200).json({ error: 'user already exists' });
-    } else {
-      fs.writeFileSync('user.json', JSON.stringify([...users, data]));
-      return res.status(200).json(data);
-    }
+    res.status(200).json({ message: 'Created user!' });
   }
 }
 
