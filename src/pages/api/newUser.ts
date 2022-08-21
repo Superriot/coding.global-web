bb2e5299320a24045d8acbce927f242483db34d3
DoniiBoyJr <doniiboyjrr@gmail.com>
2022-08-21 18:33:01 +0200

    feat: write in json

--- src/pages/api/newUser.ts
+++ src/pages/api/newUser.ts
@@ -2,11 +2,24 @@
 /* eslint-disable no-console */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import fs from 'fs';
+
+type User = { email: any; password: any; confirmpassword: any };
+
 function handler(req: any, res: any) {
   if (req.method === 'POST') {
-    const data = req.body;
+    const data = req.body as User;
 
-    res.status(200).json({ message: 'Created user!' });
+    res.status(200).json({ data });
+
+    fs.writeFileSync('user.json', JSON.stringify([data]));
+
+    // if file exist
+    if (fs.existsSync('user.json')) {
+      console.log('Datai existier');
+    } else {
+      console.log('Dateie existiert nicht');
+    }
   }
 }
 
